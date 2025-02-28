410c68a04b4d8ddb813f72760b3d1c18e6d4a548
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-02-28 05:53:15 +0200

    new: implemented UserControlSettings.

---
+++ composeApp/src/commonMain/kotlin/com/typ/sentinel/systems/ucs/UserControlSettings.kt
@@ -0,0 +1,11 @@
+package com.typ.sentinel.systems.ucs
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserControlSettings(
+    val isSentinelShieldEnabled: Boolean = true,
+    val riskSensitivityLevel: Int = 2,
+    val autoBlockHighRisk: Boolean = false,
+    val aiSuggestActionsEnabled: Boolean = true
+)
